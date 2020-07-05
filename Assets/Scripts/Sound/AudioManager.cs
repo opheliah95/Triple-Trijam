70feb4d9a04703d5fddc5c057a688bc0d2c43031
OphDev <opheliah95@gmail.com>
2020-07-06 00:18:11 +0100

    [Audio] Added audio scripts

---
+++ Assets/Scripts/Sound/AudioManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public void PlaySound(AudioClip sound)
+    {
+        GetComponent<AudioSource>().clip = sound;
+        GetComponent<AudioSource>().Play();
+    }
+}
---
+++ Assets/Scripts/Sound/SoundData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SoundData", menuName ="ScriptableObjects/Sound Data")]
+public class SoundData : ScriptableObject
+{
+    public List<AudioClip> sounds;
+}
