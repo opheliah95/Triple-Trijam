7decd486e9a547938dd22be17907af3bf7a6a58d
OphDev <opheliah95@gmail.com>
2020-07-20 17:52:18 +0100

    [INPUT] Fixed jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,12 +24,18 @@ public class PlayerController : MonoBehaviour
     Transform groundPosition, foot;
 
     [SerializeField]
-    bool canJump;
+    float groundRadius;
+    [SerializeField]
     bool _onClap;
+    [SerializeField]
+    bool _isGrounded;
     private Vector3 m_playerVelocity;
     private Camera m_Camera;
+    [SerializeField]
     float yVelocity;
     public AudioClip playerClap;
+    public SoundData jumpSounds;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -40,41 +46,45 @@ public class PlayerController : MonoBehaviour
 
     // hard code player movement
     
-    private void FixedUpdate()
+    private void Update()
     {
+        // check if player is on the ground
+        _isGrounded = GroundCheck();
+
+        // if player is on the ground and still affected by gravity
+        // stop gravity, so jump is not affected when called
+        if (_isGrounded && yVelocity < 0)
+            yVelocity = 0;
+
+        // move player around
         _PlayerMove();
+
+        // update jump
+        _PlayerJump();
+
+        // change gravity
+        yVelocity += gravity * Time.deltaTime;
 
     }
 
     private void _PlayerMove()
     {
-        float groundPos = Mathf.Abs(foot.position.y - groundPosition.position.y);
-
         if (move.sqrMagnitude < 0.01)
             return;
         var scaledMoveSpeed = moveSpeed * Time.deltaTime;
-
-        // part of velocity will be determined by jump
-        // if the character is in the air, it will fall
-        if (!controller.isGrounded)
-            yVelocity += gravity * Time.deltaTime;
-
-        // jump
-        if (canJump && groundPos < 0.1f)
-        {
-            canJump = false;
-            yVelocity = jumpSpeed * Time.deltaTime;
-        }
 
         // For simplicity's sake, we just keep movement in a single plane here. Rotate
         // direction according to world Y rotation of player.
         var moveDir = Quaternion.Euler(0, transform.eulerAngles.y, 0) * new Vector3(move.x, 0, move.y);
 
-
         // move player
         controller.Move(moveDir * scaledMoveSpeed);
-        controller.Move(transform.up* yVelocity);
 
+    }
+
+    private void _PlayerJump()
+    {
+        controller.Move(new Vector3(0, yVelocity, 0) * Time.deltaTime);
     }
 
     // I know unity event is preferred but I will stick with send messages for this jam
@@ -87,8 +97,11 @@ public class PlayerController : MonoBehaviour
     // jump function ---it is buggy right now
     void OnJump(InputValue val)
     {
-        canJump = true;
-
+        if (_isGrounded)
+        {
+            yVelocity += Mathf.Sqrt(gravity * jumpSpeed * -2f);
+        }
+       
     }
 
     // on player look
@@ -113,5 +126,22 @@ public class PlayerController : MonoBehaviour
             FindObjectOfType<AudioManager>().PlaySound(playerClap);
         else
             FindObjectOfType<AudioManager>().StopSound();
+    }
+
+    /// <summary>
+    ///  check if character is grounded
+    /// tried to use default CharacterController.isGrounded but it always return false :(
+    /// </summary>
+    /// <returns> it returns a bool value</returns>
+
+    bool GroundCheck()
+    {
+        return Physics.CheckSphere(foot.position, groundRadius, LayerMask.GetMask("Ground"), QueryTriggerInteraction.Ignore);
+    }
+
+    // visualize a list of elements like foot collider etc
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(foot.position, groundRadius);
     }
 }
